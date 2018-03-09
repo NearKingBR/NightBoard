90c48b06fcf000e93fdd2c769fed8f86200b89c0
Near <37019421+NearKingBR@users.noreply.github.com>
2018-03-09 14:30:25 -0300

    Update kick.js

--- comando/kick.js
+++ comando/kick.js
@@ -1,10 +1,11 @@
 exports.run = async(client, message, args) => {
-    //editar  >>>
+    
        if  (!message.member.hasPermissions(["KICK_MEMBERS"])) return message.reply("__**você não tem permissão para KICKAR**__");
        let reason = args.slice(1).join(' ');
        let user = message.mentions.users.first();
-       if (reason.length < 1) return message.reply('__***SELECIONE O MELIANTE!***__!');
-       if (message.mentions.users.size < 1) return message.reply('__**Você precisa de uma razão para KICKAR!**__').catch(console.error);
+       if (message.mentions.users.size < 1) return message.reply('__**Você precisa de uma razão para KICKAR!**__')
+       if (reason.length < 1) return message.reply('__***Dia o motivo***__!');
+
     
        if (!message.guild.member(user).kickable) return message.reply("__**Eu não posso KICKAR esse cara ai não, rapaz!**__");
        let member = await message.guild.member(user).kick()
