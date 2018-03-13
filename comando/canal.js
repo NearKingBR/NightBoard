9b118eb110812125b8af0d9216209c54414cb918
Near <37019421+NearKingBR@users.noreply.github.com>
2018-03-13 12:00:36 -0300

    Update canal.js

--- comando/canal.js
+++ comando/canal.js
@@ -5,12 +5,10 @@ exports.run = (client, message, args) => {
     
       if (!message.mentions.channels.first() && args.join(" ").toUpperCase() !== 'NONE') return message.channel.send('**Mecione o canal**\n > *.canal #canal*') // This returns if they don't message a channel, but we also want it to continue running if they want to disable the log
 
-    // Fetch the new channel they mentioned
     let newChannel;
-    if (args.join(" ").toUpperCase() === 'NONE') newChannel = ''; // If they wrote the word none, it sets newChannel as empty.
+    if (args.join(" ").toUpperCase() === 'NONE') newChannel = ''; 
     else newChannel = message.mentions.channels.first().id; 
 
-    // Update Channel
     db.updateText(`messageChannel_${message.guild.id}`, newChannel).then(i => {
       message.channel.send( `**Canal selecionado ${message.mentions.channels.first()}**`) 
     })
