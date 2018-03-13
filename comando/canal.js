abadc9ac35d163d4b4c021dd20746906fb3278ab
Near <37019421+NearKingBR@users.noreply.github.com>
2018-03-13 11:53:40 -0300

    Update canal.js

--- comando/canal.js
+++ comando/canal.js
@@ -1,6 +1,6 @@
 const db = require('quick.db')
 
-exports.run = (client, message, args, func) => {
+exports.run = (client, message, args) => {
 
     
       if (!message.mentions.channels.first() && args.join(" ").toUpperCase() !== 'NONE') return message.channel.send('**Mecione o canal**\n > *r!canal #canal*') // This returns if they don't message a channel, but we also want it to continue running if they want to disable the log
