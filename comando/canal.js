e31053871b6f0d7b7f3f2fa773821ce87e2f2d81
Near <37019421+NearKingBR@users.noreply.github.com>
2018-03-13 12:01:28 -0300

    Update canal.js

--- comando/canal.js
+++ comando/canal.js
@@ -2,7 +2,7 @@ const db = require('quick.db')
 
 exports.run = (client, message, args) => {
 
-    
+    if (!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply("**Você não pode usar esse comando!**");
       if (!message.mentions.channels.first() && args.join(" ").toUpperCase() !== 'NONE') return message.channel.send('**Mecione o canal**\n > *.canal #canal*') // This returns if they don't message a channel, but we also want it to continue running if they want to disable the log
 
     let newChannel;
