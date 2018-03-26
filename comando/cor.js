f74d575e8b27bdea179d655f0cb4dce6f1b2c712
Near <37019421+NearKingBR@users.noreply.github.com>
2018-03-26 12:57:12 -0300

    Update cor.js

--- comando/cor.js
+++ comando/cor.js
@@ -1,3 +1,15 @@
+const Discord = require('discord.js');
+const db = require('quick.db')
+
+  
+exports.run = (client, message, args)  => {
+  
+    const q = "401414281763094549"
+    const ownerID = "282504900552949760"
+    if(message.author.id !== ownerID)
+    if(message.author.id !== q)
+        if (!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply("**Você não pode usar esse comando!**");
+
 const size    = config.colors;
 const rainbow = new Array(size);
 
