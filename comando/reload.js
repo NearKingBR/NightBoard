658afa14afdce354f6680f680aae782005cae36a
Near <37019421+NearKingBR@users.noreply.github.com>
2018-03-30 14:53:49 -0300

    Update reload.js

--- comando/reload.js
+++ comando/reload.js
@@ -1,25 +1,10 @@
-const Discord = require('discord.js');
-const db = require('quick.db')
-
-  
-exports.run = (client, message, args)  => {
-  
-    const q = "401414281763094549"
-    const ownerID = "282504900552949760"
-    if(message.author.id !== ownerID)
-    if(message.author.id !== q)
-        if (!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply("**Você não pode usar esse comando!**");
-    
-    let reason = args.slice(0).join(' ');
-
-    if (message.author.id === "401414281763094549" || message.author.id === '401414281763094549'){
-    if (reason.length < 1) return message.reply('**Diga o comando que devo reiniciar!**');
-
-    delete require.cache[require.resolve(`./${args[0]}.js`)];
-
-    message.channel.sendMessage("**:gear: " + message.author + " Comando " + args[0] + " reiniciado!**");
-
-} else {
-    message.reply("**Sem permissão. :confused:**");
-}
-}
+const dono = "401414281763094549"
+           let reason = args.slice(0).join(' ');
+            if (message.author.id === dono) {
+        if (reason.length < 1) return message.reply('**Diga o comando que devo reiniciar!**');
+          delete require.cache[require.resolve(`./${args[0]}.js`)];
+            message.channel.sendMessage("**:gear: " + message.author + " Comando " + args[0] + " reiniciado!**");
+        } else {
+        message.reply("**Sem permissão. :confused:**");
+    }
+    }
