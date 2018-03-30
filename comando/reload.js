0a622e46b2666e5d29868243f63b9ff79da3c8e8
Near <37019421+NearKingBR@users.noreply.github.com>
2018-03-30 15:01:18 -0300

    Update reload.js

--- comando/reload.js
+++ comando/reload.js
@@ -1,17 +1,20 @@
 exports.run = (client, message, args) => {
+    
+ 
+    const r = "282504900552949760"
+    const n = "401414281763094549"
 
-
-    const q = "401414281763094549"
-    const ownerID = "282504900552949760"
-    if(message.author.id !== ownerID)
-    if(message.author.id !== q)
-        if (!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply("**Você não pode usar esse comando!**");
-           let reason = args.slice(0).join(' ');
-            if (message.author.id === dono) {
+        let reason = args.slice(0).join(' ');
+    
+ 
+  if (message.author.id === r || message.author.id === n){
         if (reason.length < 1) return message.reply('**Diga o comando que devo reiniciar!**');
-          delete require.cache[require.resolve(`./${args[0]}.js`)];
-            message.channel.sendMessage("**:gear: " + message.author + " Comando " + args[0] + " reiniciado!**");
-        } else {
+    
+        delete require.cache[require.resolve(`./${args[0]}.js`)];
+    
+        message.channel.sendMessage("**:gear: " + message.author + " Comando " + args[0] + " reiniciado!**");
+    
+    } else {
         message.reply("**Sem permissão. :confused:**");
     }
     }
