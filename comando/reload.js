5e4de60897319b0b906e476865b80fb6c7e5e8e4
Near <37019421+NearKingBR@users.noreply.github.com>
2018-03-30 15:00:12 -0300

    Update reload.js

--- comando/reload.js
+++ comando/reload.js
@@ -1,4 +1,11 @@
-const dono = "401414281763094549"
+exports.run = (client, message, args) => {
+
+
+    const q = "401414281763094549"
+    const ownerID = "282504900552949760"
+    if(message.author.id !== ownerID)
+    if(message.author.id !== q)
+        if (!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply("**Você não pode usar esse comando!**");
            let reason = args.slice(0).join(' ');
             if (message.author.id === dono) {
         if (reason.length < 1) return message.reply('**Diga o comando que devo reiniciar!**');
