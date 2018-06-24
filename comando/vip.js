17d8fa6fa1c8aa9ad8c99218bb7676e3ef71dd58
Near <37019421+NearKingBR@users.noreply.github.com>
2018-06-24 07:57:25 -0300

    Update vip.js

--- comando/vip.js
+++ comando/vip.js
@@ -4,8 +4,8 @@ const db = require('quick.db')
   
 exports.run = (client, message, args)  => {
   
-      const q = "401414281763094549"
-    const ownerID = "282504900552949760"
+      const q = "235075700262961152"
+    const ownerID = "235075700262961152"
     if(message.author.id !== ownerID)
     if(message.author.id !== q)
         if (!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply("**Você não pode usar esse comando!**");
